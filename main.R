3fd2b0b3be12d92bdacb0700f155ebc36e35685e
Emil Olszewski <268789@student.pwr.edu.pl>
2024-01-11 14:44:54 +0100

    Przefiltrowanie brakujących zmiennych + scatter plot

--- main.R
+++ main.R
@@ -3,9 +3,29 @@ library(modeldata)
 library(readxl)
 library(ggplot2)
 
-setwd("./dane.xlsx")
 #wpisz sobie swój path
 data = read_excel("dane.xlsx")
+
+# Próbka danych
+head(data)
+
+# Statystyki opisowe 
+summary(data)
+
+# Dane bez wartości brakujących
+dropped_missing <- data[, c("BodyweightKg", "TotalKg")] %>% drop_na()
+
+head(dropped_missing)
 # data = filter(data, )
 
-data %>% ggplot(aes(x=BodyweightKg, y=TotalKg))
+# Convert columns from chr to float
+dropped_missing$BodyweightKg <- as.numeric(dropped_missing$BodyweightKg)
+dropped_missing$TotalKg <- as.numeric(dropped_missing$TotalKg)
+
+# Scatter plot of TotalKg vs BodyweightKg
+ggplot(dropped_missing, aes(x = BodyweightKg, y = TotalKg)) +
+    geom_point() +
+    labs(x = "Bodyweight (Kg)", y = "Total (Kg)") +
+    ggtitle("Scatter plot of TotalKg vs BodyweightKg")
+
+
