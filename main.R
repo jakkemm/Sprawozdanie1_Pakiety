6abfc6fd612f4a9bc842f446ce2f87cf9b7addac
jakkemm <jak.kempa@gmail.com>
2024-01-11 13:34:25 +0100

    Dodanie pliku z danymi i skryptu

---
+++ main.R
@@ -0,0 +1,11 @@
+library(tidyverse)
+library(modeldata)
+library(readxl)
+library(ggplot2)
+
+setwd("/Users/jakubkempa/Documents/5 Semestr/Pakiety statystyczne/Sprawozdanie1_Pakiety")
+#wpisz sobie swój path
+
+data = read_excel("dane.xlsx")
+view(data)
+
