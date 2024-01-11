6eb57054394d37cc8fdf264ca388ebc67550c7cb
Emil Olszewski <268789@student.pwr.edu.pl>
2024-01-11 14:28:17 +0100

    Zmieniono ścieżkę

--- main.R
+++ main.R
@@ -3,9 +3,8 @@ library(modeldata)
 library(readxl)
 library(ggplot2)
 
-setwd("/Users/jakubkempa/Documents/5 Semestr/Pakiety statystyczne/Sprawozdanie1_Pakiety")
+setwd("./dane.xlsx")
 #wpisz sobie swój path
-
 data = read_excel("dane.xlsx")
 view(data)
 
